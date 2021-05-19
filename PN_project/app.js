7a9ad4a56cbd6032010326c1f0335c96f63b4c9a
Manuel <ingojedam@protonmail.com>
2021-05-19 10:27:45 -0500

    update mongodb

--- PN_project/app.js
+++ PN_project/app.js
@@ -1,3 +1,19 @@
+
+// Code for node.js, mongodb, express
+const express = require('express');
+const path = require('path');
+const routes = require('./routes/index');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use('/', routes);
+
+module.exports = app;
+
+// ORIGINAL FILE
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
