0392829786b7dceef96b3ee00a553843757fd727
Manuel <ingojedam@protonmail.com>
2021-05-23 10:47:41 -0500

    Files updated backend

--- PN_project/backend/routes/handler.js
+++ PN_project/backend/routes/handler.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Schemas = require('../models/Schemas');
+
+router.get('/addUser', async (req, res) => {
+    const user = {username: 'Manuelito', fullname: 'Manuelitooo'};
+    const newUser = new Schemas.Users(user);
+
+    try {
+        await newUser.save( async(err, newUserResult) => {
+            console.log('New user created!');
+            res.end('New user created!');
+        });
+    } catch (err) {
+        console.log(err);
+        res.end('User not added!')
+    }
+})
 
 router.get('/tweets', (req, res) => {
     const str = [{
