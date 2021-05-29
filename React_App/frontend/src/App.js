e568a7cbc7c1c43a5537b9a8144a28e46b6888d8
avasile96 <52765432+avasile96@users.noreply.github.com>
2021-05-30 01:19:29 +0200

    up

--- React_App/frontend/src/App.js
+++ React_App/frontend/src/App.js
@@ -1,30 +1,22 @@
-/*import logo from './logo.svg';*/
+//import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import Home from './components/Home';
-import {BrowserRouter as Router} from "react-router-dom";
+import Tweet from './components/Tweet';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
-  return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <Nav />
-            <Switch>
-                <Route path = "/" exact compoent = {Home} />
-                <Route path = "/tweets" exact compoent = {Tweet} />
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Nav />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/tweets" exact component={Tweet} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
