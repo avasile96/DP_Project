04279d042d60f8a2dfaa66c26058db8ed43801a6
avasile96 <52765432+avasile96@users.noreply.github.com>
2021-05-30 19:08:27 +0200

    ok

--- Node_React_Mongodb_app/backend/routes/handler.js
+++ Node_React_Mongodb_app/backend/routes/handler.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Schemas = require('../models/Schemas.js');
+
+/*// Uncomment to add a new user document to our users table
+// To use this, run the backend server, then go to URL: localhost:4000/addUser
+router.get('/addUser', async (req, res) => {
+    const user = {username: 'eaglefang', fullname: 'Sensei Johnny'};
+    const newUser = new Schemas.Users(user);
+    try {
+        await newUser.save( async(err, newUserResult) => {
+            console.log('New user created!');
+            res.end('New user created!');
+        });
+    } catch(err) {
+        console.log(err);
+        res.end('User not added!');
+    }
+});*/
+
 
 router.get('/tweets', (req, res) => {
     const str = [
